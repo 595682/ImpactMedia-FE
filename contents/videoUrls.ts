cf6207e6a7dbee829ad4bb960051e955a55efc83
Nagy Istvan <info@ist1.dev>
2022-10-14 13:30:52 +0200

    video urls modified

--- contents/videoUrls.ts
+++ contents/videoUrls.ts
@@ -1,4 +1,4 @@
-export const video = 'https://fs.impact-media.eu/hero_video_345032bdc6.mp4';
-export const anim = 'https://fs.impact-media.eu/hero_anim_de51c4f306.mp4';
-export const stream = 'https://fs.impact-media.eu/hero_7de91adf38.mp4';
-export const event = 'https://fs.impact-media.eu/hero_event_1aa29de040.mp4';
+export const video = 'https://fs.impact-media.eu/hero_video_d79802b6dc.mp4';
+export const anim = 'https://fs.impact-media.eu/hero_anim_8bf2a387cf.mp4';
+export const stream = 'https://fs.impact-media.eu/hero_724cbbd4e9.mp4';
+export const event = 'https://fs.impact-media.eu/hero_event_e292bf37ad.mp4';
