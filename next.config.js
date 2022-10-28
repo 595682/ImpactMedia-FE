6fcec6b430a0863e314a6f8eb0d26b2f81eec936
matthiastonitz <110094776+matthiastonitz@users.noreply.github.com>
2022-10-28 21:34:41 +0200

    redirect prettier

--- next.config.js
+++ next.config.js
@@ -1,34 +1,34 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 module.exports = withBundleAnalyzer({
   eslint: {
-    dirs: ['.'],
+    dirs: ["."],
   },
   poweredByHeader: false,
   trailingSlash: true,
-  basePath: '',
+  basePath: "",
   // The starter code load resources from `public` folder with `router.basePath` in React components.
   // So, the source code is "basePath-ready".
   // You can remove `basePath` if you don't need it.
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'fs.impact-media.eu'],
+    domains: ["localhost", "fs.impact-media.eu"],
   },
   redirects: async () => {
     return [
       {
-        source: '/public-relations-20',
-        destination: '/services/event-communication',
+        source: "/public-relations-20",
+        destination: "/services/event-communication",
         permanent: true,
       },
       {
-        source: '/live-streaming-webinars',
-        destination: '/services/live-stream',
+        source: "/live-streaming-webinars",
+        destination: "/services/live-stream",
         permanent: true,
       },
-    ]
-  }
+    ];
+  },
 });
