0643ffc4aaba84bc7a6ada238590f7de3877006c
Nagy Istvan <info@ist1.dev>
2022-11-07 14:46:47 +0100

    LN link added

--- src/modules/Layout/components/Footer.tsx
+++ src/modules/Layout/components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = ({
     <div className={`relative ${fullFooter ? 'bg-theme-primary' : ''} pt-20`}>
       {!noMap && (
         <>
-          <div className="absolute bottom-0 -z-10 h-1/2 w-full bg-theme-primary "></div>
+          <div className="absolute bottom-0 w-full -z-10 h-1/2 bg-theme-primary "></div>
           <Wrapper>
             <Parallax offset={90}>
               <div className="sm:px-6 lg:px-6">
@@ -49,7 +49,7 @@ const Footer = ({
           </Wrapper>
         </>
       )}
-      <footer className="mt-10 bg-theme-primary pb-6 text-white ">
+      <footer className="pb-6 mt-10 text-white bg-theme-primary ">
         <Wrapper>
           <div className="grid grid-cols-3">
             <div className="flex items-center">
@@ -61,27 +61,27 @@ const Footer = ({
                   height={100}
                 />
               </div>
-              <p className="ml-6 hidden grow text-sm lg:block">
+              <p className="hidden ml-6 text-sm grow lg:block">
                 <span className="block">Impact Media Europe SRL</span>
                 <span className="block">Rue du Monastère 10-12</span>
                 <span className="block">1000 Brussels, Belgium</span>
                 {/*  <span className="block">VAT: BE0737.709.843.</span> */}
               </p>
             </div>
-            <div className="hidden items-center  justify-center sm:flex">
-              <div className="flex w-max flex-col items-start justify-start">
-                <div className="inline-flex  items-center justify-start">
-                  <PhoneIcon className="mr-2 h-8 w-8 text-theme-secondary" />
+            <div className="items-center justify-center hidden sm:flex">
+              <div className="flex flex-col items-start justify-start w-max">
+                <div className="inline-flex items-center justify-start">
+                  <PhoneIcon className="w-8 h-8 mr-2 text-theme-secondary" />
                   <p className="text-sm text-white">(+32) 492 40 86 71</p>
                 </div>
-                <div className="inline-flex  items-center justify-start">
-                  <AtSymbolIcon className="mr-2 h-8 w-8 text-theme-secondary" />
+                <div className="inline-flex items-center justify-start">
+                  <AtSymbolIcon className="w-8 h-8 mr-2 text-theme-secondary" />
                   <p className="text-sm text-white">office@impact-media.be</p>
                 </div>
               </div>
             </div>
-            <div className="col-span-2 flex items-center justify-center sm:col-span-1">
-              <div className="inline-flex w-full items-center justify-end space-x-3">
+            <div className="flex items-center justify-center col-span-2 sm:col-span-1">
+              <div className="inline-flex items-center justify-end w-full space-x-3">
                 <div>
                   <a
                     href="https://www.facebook.com/ImpactMediaEU/"
@@ -100,17 +100,23 @@ const Footer = ({
                   </a>
                 </div>
                 <div>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="32"
-                    height="32"
-                    viewBox="0 0 24 24"
-                    className="fill-theme-secondary"
+                  <a
+                    href="https://be.linkedin.com/company/impact-media-europe"
+                    target="_blank"
+                    rel="noreferrer"
                   >
-                    <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-                  </svg>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="32"
+                      height="32"
+                      viewBox="0 0 24 24"
+                      className="fill-theme-secondary"
+                    >
+                      <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                    </svg>
+                  </a>
                 </div>
-                <div>
+                {/* <div>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="32"
@@ -120,7 +126,7 @@ const Footer = ({
                   >
                     <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z" />
                   </svg>
-                </div>
+                </div> */}
                 <div>
                   <a
                     href="https://twitter.com/ImpactMediaEU"
@@ -142,7 +148,7 @@ const Footer = ({
             </div>
           </div>
         </Wrapper>
-        <div className="mt-8 w-full text-center text-sm">
+        <div className="w-full mt-8 text-sm text-center">
           © 2022 - Impact Media Europe. All Rights Reserved.
         </div>
       </footer>
